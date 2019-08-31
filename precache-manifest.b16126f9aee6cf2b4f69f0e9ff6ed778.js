d0ef6e7fabd0b40d451bf3ab1756e2b604b589bf
Veljko <veljkos82@gmail.com>
2019-08-31 23:47:24 +0200

    Updates

---
+++ precache-manifest.b16126f9aee6cf2b4f69f0e9ff6ed778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96865a04cbde335212a41a31a5788b8b",
+    "url": "/Pomodoro/index.html"
+  },
+  {
+    "revision": "38dee16f1042614f2c68",
+    "url": "/Pomodoro/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e7fffd1dd6df8fecadca",
+    "url": "/Pomodoro/static/css/main.b98b96d6.chunk.css"
+  },
+  {
+    "revision": "38dee16f1042614f2c68",
+    "url": "/Pomodoro/static/js/2.311e5441.chunk.js"
+  },
+  {
+    "revision": "e7fffd1dd6df8fecadca",
+    "url": "/Pomodoro/static/js/main.6a9a34bd.chunk.js"
+  },
+  {
+    "revision": "c3a4169176fbcd852225",
+    "url": "/Pomodoro/static/js/runtime~main.9df9e57d.js"
+  }
+]);
