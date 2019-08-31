86281f6f52e06a5b00a26a06d34c7214d82f21de
Veljko <veljkos82@gmail.com>
2019-08-31 23:36:07 +0200

    Functionalities are all there. Needs more fine tuning.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,171 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var timer;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Pomodoro  extends React.Component {
+
+    constructor(props) {
+		super(props);
+		this.state = {
+            breakLength: 5,
+            multiplier: 25,
+            base: 1000,
+            time: 0,
+            minutes: 25,
+            seconds: 60,
+        };
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.timer = this.timer.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    componentDidMount(){
+        this.setState({
+            time: ""+(this.state.multiplier*this.state.base*60),
+            minutes: this.state.multiplier,
+            seconds: this.state.seconds === 60 ? "00" : this.state.seconds ,
+        });
+        
+    }
+
+    timer(){
+        
+        timer = setInterval(() => {
+
+            this.setState({
+                time: ""+(this.state.time-this.state.base),
+                minutes: Math.floor(((this.state.time-this.state.base)/(this.state.base))/60),
+                seconds: this.state.seconds > 0 ? this.state.seconds-1 : 59,
+            });
+
+        }, this.state.base);
+
+    }
+
+    reset(){
+        clearInterval(timer);
+        this.setState({
+            breakLength: 5,
+            multiplier: 25,
+            base: 1000,
+            time: ""+(this.state.multiplier*this.state.base*60),
+            minutes: 25,
+            seconds: 60,
+        });
+    }
+
+    increment(e){
+        console.log(e.target.id);
+        let myId = e.target.id;
+
+        if(myId==="break-increment"){
+
+            this.setState({
+                breakLength: this.state.breakLength+1,
+            });
+
+        }
+        else if(myId==="multiplier-increment"){
+
+            this.setState({
+                multiplier: this.state.multiplier+1,
+            });
+
+        }
+
+        
+
+    }
+
+    decrement(e){
+        
+        console.log(e.target.id);
+        let myId = e.target.id;
+
+        if(myId==="break-decrement"){
+
+            this.setState({
+                breakLength: this.state.breakLength > 0 ? this.state.breakLength-1 : this.state.breakLength,
+            });
+
+        }
+        else if(myId==="multiplier-decrement"){
+
+            this.setState({
+                multiplier: this.state.multiplier > 0 ? this.state.multiplier-1 : this.state.multiplier,
+            });
+
+        }
+
+    }
+
+    render(){
+        console.log(this.state);
+        let minutes = (""+this.state.minutes).length===0 ? "0"+this.state.minutes : this.state.minutes;
+        let seconds = this.state.seconds===60 ? "00" : ((""+this.state.seconds).length===1 ? "0"+this.state.seconds : this.state.seconds);
+
+        return(
+            <div className="grid-container cent">
+                <h1 className="item1">Pomodoro Clock</h1>
+
+                <div className="item2">
+                    <h3>Break Length</h3>
+
+                    <div className="arrow">
+                        <i className="fa fa-arrow-down fa-2x" id="break-decrement" onClick={this.decrement}></i>
+                    </div>
+
+                    <div className="nums">
+                        {this.state.breakLength}
+                    </div>
+
+                    <div className="arrow">
+                        <i className="fa fa-arrow-up fa-2x" id="break-increment" onClick={this.increment}></i>
+                    </div> 
+
+                </div>
+
+                <div className="item3">
+                    <h3>Session Length</h3>
+
+                    <div className="arrow">
+                        <i className="fa fa-arrow-down fa-2x" id="multiplier-decrement" onClick={this.decrement}></i>
+                    </div>
+
+                    <div className="nums">
+                        {this.state.multiplier}
+                    </div>
+
+                    <div className="arrow">
+                        <i className="fa fa-arrow-up fa-2x" id="multiplier-increment" onClick={this.increment}></i>
+                    </div> 
+
+                </div>
+
+                <div className="item4">
+                    <h3>Session</h3>
+                    <div className="nums">
+                        <label id="minutes">{minutes}</label>:<label id="seconds">{seconds}</label>
+                    </div>
+                </div>
+
+                <div className="item4">
+                    <div>
+                        <i className="fa fa-play-circle arrow controls" title="start" onClick={this.timer}></i> 
+                        <i className="fa fa-pause-circle arrow controls" title="pause"></i>
+                        <i className="fa fa-refresh fa arrow controls" title="reset" onClick={this.reset}></i>
+                    </div>
+                </div>
+
+            </div>
+        );
+
+    }
+
+}
+
+ReactDOM.render(<Pomodoro/>, document.getElementById('root'));
